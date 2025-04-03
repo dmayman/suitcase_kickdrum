50cc19f38e0153991d202547874520eee54bcf2a
dmayman <dmaymandesign@gmail.com>
2025-04-02 21:07:33 -0700

    test fsr

---
+++ src/tests/fsr_first_test.py
@@ -0,0 +1,12 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Set up I2C and ADC
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1115(i2c)
+
+# Read from channel 0
+fsr = AnalogIn(ads, ADS.P0)
